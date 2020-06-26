08b376622c2655433dd07c2e92f64459b0f302f7
huajiao925 <lyrali142857@outlook.com>
2020-06-26 19:07:36 +0800

    department.java

---
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/model/Department.java
@@ -0,0 +1,60 @@
+package sg.edu.LeaveApplication.model;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Department {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private String name;
+	private Integer minmumCount;
+	@OneToMany(mappedBy="department")
+	private Collection<User> users;
+	public Department() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Department(String name, Integer minmumCount, Collection<User> users) {
+		super();
+		this.name = name;
+		this.minmumCount = minmumCount;
+		this.users = users;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getMinmumCount() {
+		return minmumCount;
+	}
+	public void setMinmumCount(Integer minmumCount) {
+		this.minmumCount = minmumCount;
+	}
+	public Collection<User> getUsers() {
+		return users;
+	}
+	public void setUsers(Collection<User> users) {
+		this.users = users;
+	}
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + ", minmumCount=" + minmumCount + "]";
+	}
+	
+	
+}
