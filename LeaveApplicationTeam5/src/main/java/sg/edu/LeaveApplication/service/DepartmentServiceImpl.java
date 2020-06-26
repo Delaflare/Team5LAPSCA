1d94d14fab8cf8da25ab022fc28b17a85036e306
Delaflare <49422455+Delaflare@users.noreply.github.com>
2020-06-27 02:12:53 +0800

    debug previous fix

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/service/DepartmentServiceImpl.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/service/DepartmentServiceImpl.java
@@ -2,50 +2,75 @@ package sg.edu.LeaveApplication.service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import sg.edu.LeaveApplication.model.Department;
+import sg.edu.LeaveApplication.repo.DepartmentRepository;
 
 public class DepartmentServiceImpl implements DepartmentService {
 
+	@Autowired
+	DepartmentRepository drepo;
+	
 	@Override
 	public ArrayList<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Department>list = (ArrayList<Department>)drepo.findAll();
+		return list;
 	}
 
 	@Override
 	public boolean saveDepartment(Department dept) {
 		// TODO Auto-generated method stub
-		return false;
+		 if (drepo.save(dept) != null)
+		 {
+			 return true;
+		 }
+		 else
+		 {
+			 return false;
+		 }
+	
 	}
 
 	@Override
 	public boolean editDepartment(Department dept) {
-		// TODO Auto-generated method stub
-		return false;
+		 if (drepo.save(dept) != null)
+		 {
+			 return true;
+		 }
+		 else
+		 {
+			 return false;
+		 }
 	}
 
 	@Override
 	public void deleteDepartment(Department dept) {
 		// TODO Auto-generated method stub
-
+		drepo.delete(dept);
 	}
 
 	@Override
 	public ArrayList<String> findAllDepartmentNames() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<String> dept = drepo.findAllDepartmentNames();
+		return dept;
 	}
 
 	@Override
 	public Department findDeparmentByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Department> list = (ArrayList <Department>) drepo.findByName(name);
+		
+		Department dept		 = list.get(0); // get the whole list from 0
+		
+		return dept;
 	}
 
 	@Override
 	public Department findDeparmentById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Department dept = drepo.findById(id).get();
+		return dept ;
 	}
-
+	
 }
+
