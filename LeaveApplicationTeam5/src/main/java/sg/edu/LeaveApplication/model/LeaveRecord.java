1c978f4c2b4650cb9ef9eac897435d47b2f30ba9
huajiao925 <lyrali142857@outlook.com>
2020-06-28 07:52:15 +0800

    applyLeave + Leave list (emp)_1stCommit

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/model/LeaveRecord.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/model/LeaveRecord.java
@@ -11,29 +11,97 @@ import javax.validation.constraints.NotEmpty;
 public class LeaveRecord {
 	@Id
 	@GeneratedValue( strategy = GenerationType.AUTO)
-	private int id;
-	@NotEmpty
-	private int userId;
-	@NotEmpty
-	private int LeaveTypeId;
-	@NotEmpty
+	private Integer id;
+	private Integer userId;  
+	private Integer LeaveTypeId;
 	private Date startDate;
-	@NotEmpty
-	private int duration;
-	@NotEmpty
+	private Integer duration;
 	private Status status;
-	@NotEmpty
 	private String description;
-	@NotEmpty
 	private String workDissemination;
-	@NotEmpty
 	private String contactDetails;
-	@NotEmpty
 	private String comments;
-	@NotEmpty
-	private int deptId;
-	@NotEmpty
+	private Integer deptId;
 	private Date leaveAppliedDate;
-	@NotEmpty
 	private Date leaveApprovedDate;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getLeaveTypeId() {
+		return LeaveTypeId;
+	}
+	public void setLeaveTypeId(Integer leaveTypeId) {
+		LeaveTypeId = leaveTypeId;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Integer getDuration() {
+		return duration;
+	}
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getWorkDissemination() {
+		return workDissemination;
+	}
+	public void setWorkDissemination(String workDissemination) {
+		this.workDissemination = workDissemination;
+	}
+	public String getContactDetails() {
+		return contactDetails;
+	}
+	public void setContactDetails(String contactDetails) {
+		this.contactDetails = contactDetails;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public Integer getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public Date getLeaveAppliedDate() {
+		return leaveAppliedDate;
+	}
+	public void setLeaveAppliedDate(Date leaveAppliedDate) {
+		this.leaveAppliedDate = leaveAppliedDate;
+	}
+	public Date getLeaveApprovedDate() {
+		return leaveApprovedDate;
+	}
+	public void setLeaveApprovedDate(Date leaveApprovedDate) {
+		this.leaveApprovedDate = leaveApprovedDate;
+	}
+	
 }
--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/repo/LeaveTypeRepository.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/repo/LeaveTypeRepository.java
@@ -1,9 +1,14 @@
 package sg.edu.LeaveApplication.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sg.edu.LeaveApplication.model.LeaveTypes;
 
 public interface LeaveTypeRepository extends JpaRepository<LeaveTypes, Integer> {
+
+	ArrayList<LeaveTypes> findAll();
+
 
 }
