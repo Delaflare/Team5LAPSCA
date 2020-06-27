3159102258dba1c0eaccf9d322dc83d47a7ff943
Delaflare <49422455+Delaflare@users.noreply.github.com>
2020-06-28 02:52:16 +0800

    LeaveController Commit

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/controller/LeaveController.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/controller/LeaveController.java
@@ -1,8 +1,41 @@
 package sg.edu.LeaveApplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@RequestMapping("/Leave")
+@SessionAttributes("user")
 public class LeaveController {
-
+	
+	
+	@GetMapping("/approval")
+		public String ApproveLeave()
+		{
+			
+			return "managerHome";
+		}
+		
+		public String RejectLeave()
+		{
+			
+			
+			return "managerHome";
+		}
+		
+		public String ViewLeaveRequest()
+		{
+			// get the individual Leave request
+			return "managerViewLeave";
+		}
+		
+		public String GetLeaveRequest()
+		{
+			//print out all the leave
+			return "managerLeaveList";
+		}
+	
+	
 }
