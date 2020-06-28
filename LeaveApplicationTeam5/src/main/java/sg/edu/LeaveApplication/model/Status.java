d0dbfdc60264e9948b3f9e391bb0902b1076d0ce
huajiao925 <lyrali142857@outlook.com>
2020-06-28 11:43:15 +0800

    bug

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/model/Status.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/model/Status.java
@@ -1,11 +1,6 @@
 package sg.edu.LeaveApplication.model;
 
 public enum Status {
-<<<<<<< HEAD
 
 PENDING, APPROVED, REJECTED, CANCELLED
-
-=======
-	PENDING, APPROVED, REJECTED
->>>>>>> branch 'master' of https://github.com/Delaflare/Team5LAPSCA.git
 }
