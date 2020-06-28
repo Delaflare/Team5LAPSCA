4b212b6b6a5b8bddcc9c8bd39162cb38d8b3850a
huajiao925 <lyrali142857@outlook.com>
2020-06-28 20:59:05 +0800

    cleanup_LeaveControl

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/controller/LeaveController.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/controller/LeaveController.java
@@ -3,15 +3,7 @@ package sg.edu.LeaveApplication.controller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 import javax.validation.Valid;
 
@@ -26,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import sg.edu.LeaveApplication.model.LeaveRecord;
-import sg.edu.LeaveApplication.model.PublicHolidays;
 import sg.edu.LeaveApplication.model.Status;
 import sg.edu.LeaveApplication.service.LeaveService;
 import sg.edu.LeaveApplication.service.LeaveTypeService;
@@ -56,28 +47,7 @@ public class LeaveController {
 	public void setUservice(UserService uservice) {
 		this.uservice = uservice;
 	}
-	/*
-	@Autowired
-	//get all holidays and insert as param
 	
-	
-	public Integer getDuration(String sd, String ed, ArrayList<PublicHolidays> holidays) {
-				
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		LocalDate date1= LocalDate.parse(sd, formatter);  
-		LocalDate date2= LocalDate.parse(ed, formatter); 
-		
-		Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY
-				&& date.getDayOfWeek() == DayOfWeek.SUNDAY;
-		
-		//Predicate<LocalDate> isHolidy = date -> date.holidays.isPresent() ? holidays.get().contains(date): false;
-		long daysBetween = ChronoUnit.DAYS.between(date1, date2);
-		long duration = Stream.iterate(sd, date->date.plusDays(1))
-				.limit(daysBetween)
-				.filter(isHolidy.or(isWeekend).negate().count());
-		return duration;
-	}
-	*/
 	
 	@RequestMapping("/list")
 	public String list(Model model) {
@@ -91,7 +61,7 @@ public class LeaveController {
 		
 		model.addAttribute("leave", new LeaveRecord());
 		model.addAttribute("leaveTypes", leavetypeservice.findAll());
-		model.addAttribute("balance", 2);//will replace
+		model.addAttribute("balance", 2); //will replace
 		return "createLeave";
 	}
 
@@ -124,7 +94,7 @@ public class LeaveController {
 	@RequestMapping("/update/{id}")
 	public String updateLeave(@PathVariable("id") Integer id, Model model) {
 		model.addAttribute("leaveTypes", leavetypeservice.findAll());
-		LeaveRecord lr = leaveservice.findLeaveRecordByID(id);
+		LeaveRecord lr = leaveservice.findLeaveRecordById(id);
 		//only when Pending, allow update
 		if(lr != null && lr.getStatus()==Status.PENDING || lr.getStatus()==Status.UPDATED) {
 			model.addAttribute("leave", lr);
@@ -135,13 +105,13 @@ public class LeaveController {
 	
 	@RequestMapping("/detail/{id}")
 	public String viewLeave(@PathVariable("id") Integer id, Model model) {
-		model.addAttribute("leave", leaveservice.findLeaveRecordByID(id));
+		model.addAttribute("leave", leaveservice.findLeaveRecordById(id));
 		return"leaveDetails";
 	}
 	
 	@RequestMapping("/delete/{id}")
 	public String deleteLeave(@PathVariable("id") Integer id, Model model) {
-		LeaveRecord lr = leaveservice.findLeaveRecordByID(id);
+		LeaveRecord lr = leaveservice.findLeaveRecordById(id);
 		//only when Pending, allow delete
 		if(lr !=null && lr.getStatus()==Status.PENDING || lr.getStatus()==Status.UPDATED) {
 			leaveservice.deleteLeave(lr);
@@ -151,7 +121,7 @@ public class LeaveController {
 
 	@RequestMapping("/cancel/{id}")
 	public String cancelLeave(@PathVariable("id") Integer id, Model model) {
-		LeaveRecord lr = leaveservice.findLeaveRecordByID(id);
+		LeaveRecord lr = leaveservice.findLeaveRecordById(id);
 		//have record and before approved, allow cancel
 		if(lr !=null && lr.getStatus()!= Status.APPROVED) {
 			leaveservice.cancelLeave(lr);
