7602be7341f69d691bea17de1ef45118dfbe4fd9
huajiao925 <lyrali142857@outlook.com>
2020-06-28 21:47:14 +0800

    cleanup_leaveapp.jave

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/LeaveApplication.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/LeaveApplication.java
@@ -1,13 +1,9 @@
 package sg.edu.LeaveApplication;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import sg.edu.LeaveApplication.model.User;
 import sg.edu.LeaveApplication.repo.UserRepository;
 
 @SpringBootApplication
@@ -20,17 +16,5 @@ public class LeaveApplication {
 		SpringApplication.run(LeaveApplication.class, args);
 		
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	return args -> {
-		
-		User u1 = new User("first", "last", "user", "pwd123", "abc@email.com");
-		urepo.save(u1);
-		
-		//Department d1 = new Department("Dept1", 2, u1);	
-		
-	};
-
-	
-}}
+ 
+}
