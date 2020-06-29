90303f881f9f90955124416a559821f836f98b9c
huajiao925 <lyrali142857@outlook.com>
2020-06-29 14:17:52 +0800

    clean up

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/LeaveApplication.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/LeaveApplication.java
@@ -1,17 +1,22 @@
 package sg.edu.LeaveApplication;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+import sg.edu.LeaveApplication.model.User;
 import sg.edu.LeaveApplication.repo.UserRepository;
 
 @SpringBootApplication
 public class LeaveApplication {
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LeaveApplication.class, args);
 		
 	}
- 
+	
 }
