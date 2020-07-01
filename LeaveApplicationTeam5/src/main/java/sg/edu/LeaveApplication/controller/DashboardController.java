3b97c377258765cc61097c4503e857431ed2c7e8
catintheclouds <makebelief@icloud.com>
2020-07-01 10:55:50 +0800

    Alittle changes to OTservice and views

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/controller/DashboardController.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/controller/DashboardController.java
@@ -1,8 +1,36 @@
 package sg.edu.LeaveApplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sg.edu.LeaveApplication.service.LeaveService;
+import sg.edu.LeaveApplication.service.OTService;
 
 @Controller
+@RequestMapping(value = "/dashboard")
 public class DashboardController {
 	
+	@Autowired
+	LeaveService leaveservice;
+	@Autowired
+	public void setLeaveservice(LeaveService leaveservice) {
+		this.leaveservice = leaveservice;
+	}
+	
+	@Autowired
+	OTService OTservice;
+	@Autowired
+	public void setOTservice(OTService OTservice) {
+		this.OTservice = OTservice;
+	}
+	
+	@RequestMapping(value = "/home")
+	public String pendingLeave(Model model)
+	{
+		model.addAttribute("pendingLeave", leaveservice.findAllPendingLeave());
+		model.addAttribute("pendingOT", OTservice.findAllPendingOT());
+		return "HomeDashboard";
+	}
 }
--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/service/OTService.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/service/OTService.java
@@ -1,5 +1,19 @@
 package sg.edu.LeaveApplication.service;
 
+import java.util.ArrayList;
+
+import sg.edu.LeaveApplication.model.OTRecord;
+
 public interface OTService {
+	public ArrayList<OTRecord> findAll();
+	public ArrayList<OTRecord> findAllPendingOT();
 	
+//	public LeaveRecord findLeaveRecordById(Integer id);
+//	public boolean saveLeave(LeaveRecord leaverecord);
+//	public boolean Approve(Integer id, String comment);
+//	public void deleteLeave(LeaveRecord lr);
+//	public void cancelLeave(LeaveRecord lr);
+//	public boolean Reject(Integer id, String comment);
+//	public ArrayList<String> findAllLeaveStatusName();
+//	public ArrayList<LeaveRecord> findLeaveByEmployeeAndLeave(String keyword, String ltName);
 }
