5248dabab774670f40777b2d8765f00390961439
ssoe11 <60837279+ssoe11@users.noreply.github.com>
2020-07-02 15:59:28 +0800

    Update OT repo,service, controller for manager

--- LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/repo/OTRepository.java
+++ LeaveApplicationTeam5/src/main/java/sg/edu/LeaveApplication/repo/OTRepository.java
@@ -19,4 +19,11 @@ public interface OTRepository extends JpaRepository<OTRecord, Integer> {
 	
 	@Query("Select l from OTRecord l where l.user = :user")
 	ArrayList<OTRecord> findByUser(@Param("user") User user);
+	
+	@Query("Select l from OTRecord l where l.status=0 OR l.status='PENDING' OR l.status= 4 OR l.status='UPDATED'")
+	ArrayList<OTRecord> findAllPendingAndUpdatedOT();
+	
+	@Query("SELECT l from OTRecord l JOIN l.user u WHERE (u.firstName LIKE %:keyword% OR u.lastName LIKE %:keyword%)"
+			+ " AND (l.status=0  OR l.status= 4)")
+	ArrayList<OTRecord> findPendingOTByUser(@Param("keyword") String keyword);
 }
